58c5096924fdae4b5d984aaad780f12bc12cfd55
Gonzalo J. Carracedo Carballal <gcarracedo@cab.inta-csic.es>
2023-01-23 12:24:10 +0100

    Add default licensing headers

--- simui/SimUiWindow.py
+++ simui/SimUiWindow.py
@@ -1,7 +1,37 @@
+#
+# Copyright (c) 2023 Gonzalo J. Carracedo <BatchDrake@gmail.com>
+#
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
 from PyQt6 import QtCore, uic
 from PyQt6.QtCore import pyqtSignal, Qt
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QMessageBox, QDialogButtonBox, QFileDialog,  QGraphicsScene, QGraphicsView
+from PyQt6.QtWidgets import QMessageBox, QDialogButtonBox, QFileDialog
 from PyQt6.QtSvgWidgets import QSvgWidget
 from radiosim import SimulationConfig, DetectorConfig
 from .PlotWidget import PlotWidget
